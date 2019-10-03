625aba3c7e45716c6d8d1171f7ee097e786b856b
BiserBorisov <butoff@abv.bg>
2019-10-03 22:50:08 +0300

    Add projects to the application skeleton

---
+++ Cinema.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cinema.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Cinema.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cinema.Entities
+{
+    public class Class1
+    {
+    }
+}
